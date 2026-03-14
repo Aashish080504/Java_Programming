f0b9b3f7e2d3c95fad32b7fb60c2c5b3cb9a4945
Aashish Labade <aashishlabade2004@gmail.com>
2026-03-14 11:16:38 +0530

    Add files via upload

---
+++ Assignments/Assignment_72/Program_03.java
@@ -0,0 +1,112 @@
+import java.util.*;
+
+class Matrix
+{
+    public int iRow;
+    public int iCol;
+    public int Arr[][];
+
+    public Matrix(int a, int b)
+    {
+        this.iRow = a;
+        this.iCol = b;
+
+        Arr = new int[iRow][iCol];
+    }
+
+    public void Accept()
+    {
+        int i = 0, j = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the elements of matrix");
+
+        for(i = 0; i < iRow; i++)
+        {
+            System.out.println("Enter the elements of row : "+(i+1));
+
+            for(j = 0; j < iCol; j++)
+            {
+                Arr[i][j] = sobj.nextInt();
+            }
+        }
+    }
+
+    public void Display()
+    {
+        int i = 0, j = 0;
+
+        System.out.println("Elements of matrix are : ");
+
+        for(i = 0; i < iRow; i++)
+        {
+            for(j = 0; j < iCol; j++)
+            {
+                System.out.print(Arr[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public int MaxDiagonal()
+    {
+        int i = 0, j = 0;
+        int iLarge1 = Arr[0][0];     
+        int iLarge2 = Arr[0][iCol-1];
+
+        for(i = 0; i < iRow; i++)
+        {
+            for(j = 0; j < iCol; j++)
+            {
+                if(i == j)
+                {
+                    if(Arr[i][j] > iLarge1)
+                    {
+                        iLarge1 = Arr[i][j];
+                    }
+                }
+
+                if(i + j == iCol - 1)
+                {
+                    if(Arr[i][j] > iLarge2)
+                    {
+                        iLarge2 = Arr[i][j];
+                    }
+                }
+            }
+        }
+
+        if(iLarge1 > iLarge2)
+        {
+            return iLarge1;
+        }
+
+        return iLarge2;
+    }
+
+}
+
+class Program_03
+{
+    public static void main(String A[])
+    {
+        int iRow = 0, iCol = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of Rows : ");
+        iRow = sobj.nextInt();
+
+        System.out.println("Enter the number of Columns : ");
+        iCol = sobj.nextInt();
+
+        Matrix mobj = new Matrix(iRow,iCol);
+
+        mobj.Accept();
+        mobj.Display();
+
+        System.out.println("Largest number from both diagonal is : "+mobj.MaxDiagonal());
+
+        sobj.close();
+    }
+}
